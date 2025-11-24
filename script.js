eecde1997fcc24665e0f50387a2608d4e4de5ce1
VV104-24 <shierobts1230@gmail.com>
2025-11-24 11:51:14 +0700

    update JS

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const heading = document.querySelector('h1')
+const originalText = heading.textContent // เก็บข้อความตั้งต้นไว้
+
 function changeTitle() {
-  heading.textContent = 'You changed the title!'
+  if (heading.textContent === originalText) {
+    // ถ้าข้อความปัจจุบันคือข้อความตั้งต้น
+    heading.textContent = 'You changed the title!'
+  } else {
+    // ถ้าข้อความปัจจุบันถูกเปลี่ยนไปแล้ว (คือ 'You changed the title!')
+    heading.textContent = originalText
+  }
 }
